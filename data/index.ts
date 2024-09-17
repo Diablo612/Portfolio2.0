fae1411fda95f090c6937815a92d7e41780324b3
Aditya Rishi <adityarishi4949@gmail.com>
2024-09-17 15:31:20 +0530

    Final

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4",
+      imgClassName: "w-full h-full blur-sm",
+      titleClassName: "justify-center",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "blur-sm",
+      titleClassName: "justify-center",
+      img: "/id3.jpg",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building new projects everyday",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 3,
+      title: "AI Podcast SaaS Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Zoom Clone",
+      des: "Recreated the Zoom Clone for Android and iOS devices to video conferenceing. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "I have completed an NPTEL certification in Database Management Systems (DBMS), which provided me with a strong foundational understanding of database concepts, design, and management. The course covered key topics such as relational databases, SQL queries, normalization, transaction management, and indexing techniques. Through a combination of lectures, assignments, and quizzes, I gained practical skills in handling databases, ensuring data integrity, and optimizing query performance. This certification has not only enhanced my technical proficiency but also reinforced my problem-solving abilities, which are crucial for building efficient and scalable applications.",
+      name: "NPTEL",
+      title: "National Programme on Technology Enhanced Learning",
+    },
+    {
+      quote:
+        "I have earned a certification from NPTEL in C, which has greatly enhanced my understanding and skills in this crucial programming language. Through the course, I delved into both fundamental and advanced topics, from basic syntax and object-oriented programming to intricate concepts like memory management and templates. The certification process involved rigorous coursework, practical assignments, and exams, ensuring that I gained both theoretical knowledge and hands-on experience. This credential not only bolsters my resume but also underscores my commitment to mastering complex programming concepts, making it a valuable asset as I pursue opportunities in software development and engineering.",
+        name: "NPTEL",
+        title: "National Programme on Technology Enhanced Learning",
+    },
+    {
+      quote:
+        "I have completed a Java certification from Coursera, which has significantly deepened my knowledge and expertise in this widely-used programming language. The course covered a broad range of topics, from foundational Java concepts and syntax to more advanced areas such as concurrency, data structures, and design patterns. The comprehensive curriculum included hands-on projects and assessments that allowed me to apply what I learned in real-world scenarios. Earning this certification not only strengthens my resume but also highlights my dedication to mastering Java, making me well-prepared for roles in software development and programming",
+        name: "NPTEL",
+        title: "National Programme on Technology Enhanced Learning",
+    },
+    {
+      quote:
+        "I have also completed a ReactJS course on Udemy, which has greatly expanded my skills in building dynamic and interactive user interfaces. The course offered an in-depth exploration of ReactJS fundamentals, including components, state management, hooks, and the component lifecycle. Through a series of hands-on projects and exercises, I was able to apply these concepts to real-world scenarios, enhancing my ability to create efficient and responsive web applications. This certification from Udemy not only reinforces my expertise in ReactJS but also equips me with practical experience that is valuable for developing modern web applications",
+      name: "Udemy",
+      title: "Udemy is an online learning platform offering a wide range of courses.",
+    },
+    {
+      quote:
+        "I recently completed courses on PostgreSQL and MongoDB from Udemy, which have significantly advanced my database management skills. The PostgreSQL course provided a thorough understanding of relational database principles, including advanced querying, indexing, and transaction management, while the MongoDB course focused on NoSQL concepts such as document-oriented storage, schema design, and performance optimization. Both courses included practical exercises and real-world scenarios, allowing me to gain hands-on experience with these powerful databases. This training has enhanced my ability to design, manage, and optimize databases for various applications, making me well-equipped for roles involving complex data management and analysis",
+        name: "Udemy",
+        title: "Udemy is an online learning platform offering a wide range of courses.",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "NextJS",
+      img: "/next.svg",
+      nameImg: "",
+    },
+    {
+      id: 2,
+      name: "TailwindCSS",
+      img: "/tail.svg",
+      nameImg: "",
+    },
+    {
+      id: 3,
+      name: "TypeScript",
+      img: "/ts.svg",
+      nameImg: "",
+    },
+    {
+      id: 4,
+      name: "React",
+      img: "/re.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "Framer",
+      img: "/fm.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native and Flutter.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Personal Web and App Dev Project",
+      desc: "Developed many projects on Next.js and React.js for complete hands on experience of them both.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Practicing Frontend Developer",
+      desc: "Developed, maintained and still learning better user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      href:"https://github.com/Diablo612",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      href:"",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      href:"https://www.linkedin.com/in/aditya-rishi-20473b246/",
+    },
+  ];
